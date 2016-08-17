71e598b292ecebd4b969150fbc1b88a967320827
Kimo <jimtrinh9985@gmail.com>
2016-08-16 22:30:17 -0700

    Initial commit

---
+++ wear/src/main/java/com/jimtrinh9985gmail/vibe/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package com.jimtrinh9985gmail.vibe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.jimtrinh9985gmail.vibe.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
